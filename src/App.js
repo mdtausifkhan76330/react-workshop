84caeaae6dbcd6430ffe529494973805c4860c6f
mdtausifkhan76330 <tausifkhan0336@gmail.com>
2023-02-23 13:41:06 +0530

    Day 4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Navbar from '../src/Component/Navbar'
+import Body from './Component/Body';
+import TempComp from './Component/temp/TempComp';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Body />
+      <TempComp/>
     </div>
   );
 }
---
+++ src/Component/Body.jsx
@@ -0,0 +1,23 @@
+import './Body.css';
+const Body=()=>{
+    return(<div id='body-container'>
+        
+        <div className="box">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel labore explicabo ea assumenda dolores in, asperiores dolorem soluta corrupti veniam est ipsa sint, incidunt eaque, vitae id? Dolorem, ipsum velit.
+        </div>
+        <div className="box">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel labore explicabo ea assumenda dolores in, asperiores dolorem soluta corrupti veniam est ipsa sint, incidunt eaque, vitae id? Dolorem, ipsum velit.
+        </div>
+        <div className="box">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel labore explicabo ea assumenda dolores in, asperiores dolorem soluta corrupti veniam est ipsa sint, incidunt eaque, vitae id? Dolorem, ipsum velit.
+        </div><div className="box">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel labore explicabo ea assumenda dolores in, asperiores dolorem soluta corrupti veniam est ipsa sint, incidunt eaque, vitae id? Dolorem, ipsum velit.
+        </div>
+    </div>
+    
+        
+
+    )
+    }
+
+    export default Body;
